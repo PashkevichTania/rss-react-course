07cecafed6f0058bcb120c181cd8e4e084215fc6
PashkevichTania <pashkevichtania2004@gmail.com>
2021-08-22 16:04:57 +0300

    feat: configured jest

---
+++ app/tests/main.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import App from '../src/App';
+import {test} from "@jest/globals";
+
+test('Link changes the class when hovered', () => {
+
+  expect(0).toBe(0)
+});
